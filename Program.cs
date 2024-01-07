4663bdcf3b4eebde44d01a575deac04098d5f1cc
Ana P Batista <95220408+anagit13@users.noreply.github.com>
2024-01-07 13:42:07 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TESTE DO SMARTPHONE IPHONE
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "482956", modelo: "Modelo Y", imei: "777777777", memoria: "64");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("\n");
+
+// TESTE DO SMARTPHONE NOKIA
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "569178", modelo: "Modelo X", imei: "888888888", memoria: "32"); 
+nokia.Ligar(); 
+nokia.InstalarAplicativo("Canva");
