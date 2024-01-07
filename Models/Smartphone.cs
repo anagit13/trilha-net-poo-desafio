c9cd737d88b2d5aa2773153270b61efc2b2c6326
Ana P Batista <95220408+anagit13@users.noreply.github.com>
2024-01-07 13:41:02 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,24 +3,27 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public string Memoria { get; set; }
+        // IMPLEMENTADO !!        
+ 
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            // IMPLEMENTADO !!          
         }
-
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
-
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
