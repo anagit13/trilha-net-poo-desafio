0105e93e75159769be218bd4ddf6e00266cdbfa8
Ana P Batista <95220408+anagit13@users.noreply.github.com>
2024-01-07 13:40:14 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // IMPLEMENTADO !!
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, string memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+    // IMPLEMENTADO !!
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando aplicativo {0} no Nokia", nomeApp);
+        }
     }
 }
